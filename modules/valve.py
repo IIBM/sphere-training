21c7656b81cd1678ee86070830b8a10dbaa75a8c
aimc <aimc@ubuntu.(none)>
2014-01-12 22:00:54 -0300

    Transformed class into Singleton to prevent it from being created twice.

--- modules/valve.py
+++ modules/valve.py
@@ -17,32 +17,80 @@ class dummypp () :
         self.data = data
         return self.data
   
-  
-class Valve() :
-    def __init__(self) :
-        try :
-          logger.info('New instance of valve')
-          import parallel
-          self.p = parallel.Parallel()
-        except :
-          logger.warning('Could not find any parallel port. Using dummy parallel port')
-          self.p = dummypp()
 
+
+
+class Valve(object):
+  ### singleton inner class: valve.
+  class __Valve:
+    def __init__(self):
+      self.val = None
+      #print "init valve."
+      try :
+           logger.info('New instance of valve')
+           import parallel
+           self.p = parallel.Parallel()
+      except :
+           logger.warning('Could not find any parallel port. Using dummy parallel port')
+           self.p = dummypp()
+    def __str__(self):
+      return repr(self)
     def open(self) :
-        logger.info('Valve opened')
-        a = self.p.getData()
-        return self.p.setData(a|ValvePinMask)
-
+         logger.info('Valve opened')
+         a = self.p.getData()
+         return self.p.setData(a|ValvePinMask)
     def close(self) :
-        logger.info('Valve closed')
-        a = self.p.getData()
-        return self.p.setData(a&(~ValvePinMask))
-    
+         logger.info('Valve closed')
+         a = self.p.getData()
+         return self.p.setData(a&(~ValvePinMask))
+     
     def drop(self) :
-        logger.info('Valve drop')
-        self.open()
-        time.sleep(DropTime)
-        self.close()
+         logger.info('Valve drop')
+         self.open()
+         time.sleep(DropTime)
+         self.close()
+  ###
+  instance = None
+  def __new__(cls): # __new__ always a classmethod
+    if not Valve.instance:
+      Valve.instance = Valve.__Valve()
+    return Valve.instance
+  def __getattr__(self, name):
+    return getattr(self.instance, name)
+  def __setattr__(self, name):
+    return setattr(self.instance, name)
+
+
+
+
+
+#===============================================================================
+# class Valve() :
+#     def __init__(self) :
+#         try :
+#           logger.info('New instance of valve')
+#           import parallel
+#           self.p = parallel.Parallel()
+#         except :
+#           logger.warning('Could not find any parallel port. Using dummy parallel port')
+#           self.p = dummypp()
+# 
+#     def open(self) :
+#         logger.info('Valve opened')
+#         a = self.p.getData()
+#         return self.p.setData(a|ValvePinMask)
+# 
+#     def close(self) :
+#         logger.info('Valve closed')
+#         a = self.p.getData()
+#         return self.p.setData(a&(~ValvePinMask))
+#     
+#     def drop(self) :
+#         logger.info('Valve drop')
+#         self.open()
+#         time.sleep(DropTime)
+#         self.close()
+#===============================================================================
 
 if __name__ == '__main__':
     # create a logging format
@@ -56,6 +104,14 @@ if __name__ == '__main__':
     v1.open()
     time.sleep(2)
     v1.close()
+    print v1
     time.sleep(2)
     v1.close()
+    print v1
+    v2 = Valve()
+    v2.open()
+    time.sleep(2)
+    v2.close()
+    print v2
+    
     logging.info('End Valve Test')
