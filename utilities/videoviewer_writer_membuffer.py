cc9141d51195922b08cbe7293ea885dfa3afcb31
alfredo <aimc.234@gmail.com>
2017-12-23 20:15:22 -0300

    Utilitarios: Header con comentarios descriptivos sobre funcionamiento. Cambio encoding por defecto.

--- utilities/videoviewer_writer_membuffer.py
+++ utilities/videoviewer_writer_membuffer.py
@@ -1,3 +1,12 @@
+'''
+python script that reads a video (or camera) and writes output to file. It does so by buffering each frame into memory and then writing the saved frames into disk.
+created for testing purposes.
+
+usage:
+videoviewer_writer_membuffer.py input_video_file.avi output_video_file.avi
+
+part of sphere-training repo: https://github.com/IIBM/sphere-training/
+'''
 import numpy as np
 import cv2
 import sys
@@ -10,6 +19,12 @@ if (len(sys.argv)) > 1:
     input_video = sys.argv[1]
     if (len(sys.argv)) > 2:
         output_video = sys.argv[2]
+else:
+    print "Not enough arguments."
+    print """usage:
+    videoviewer_writer_membuffer.py input_video_file.avi output_video_file.avi
+    """
+    sys.exit()
 
 print "input video: ", input_video
 if (output_video == ""):
@@ -22,17 +37,21 @@ if (len(input_video) == 1):
     input_video = int(input_video)
     print "Input regarded as camera #", input_video
 cap = cv2.VideoCapture( input_video )
+#lossless codec: FFV1 (see: https://docs.opencv.org/3.3.0/dd/d9e/classcv_1_1VideoWriter.html)
 # Define the codec and create VideoWriter object
-#fourcc = cv2.VideoWriter_fourcc(*'PIM1') #1.0MB
-fourcc = cv2.VideoWriter_fourcc(*'MJPG') #6.4MB
-#fourcc = cv2.VideoWriter_fourcc(*'DIV3') #3.1MB
-#fourcc = cv2.VideoWriter_fourcc(*'DIVX') #0.9MB
+##fourcc = cv2.VideoWriter_fourcc(*'PIM1') #1.0MB
+#fourcc = cv2.VideoWriter_fourcc(*'MJPG') #6.4MB
+##fourcc = cv2.VideoWriter_fourcc(*'DIV3') #3.1MB
+##fourcc = cv2.VideoWriter_fourcc(*'DIVX') #0.9MB
+fourcc = cv2.VideoWriter_fourcc(*'FFV1') #3.7MB
 
 capturedWidth = int(cap.get(3))
 capturedHeight = int(cap.get(4))
+capturedFPS = int(cap.get(5))
 
-out = cv2.VideoWriter(output_video,fourcc, 20.0, (capturedWidth,capturedHeight))
-#out = cv2.VideoWriter('output.avi', -1, 20.0, (640,480))
+
+out = cv2.VideoWriter(output_video,fourcc, capturedFPS, (capturedWidth,capturedHeight))
+#out = cv2.VideoWriter(output_video,0, 0, (capturedWidth,capturedHeight))
 
 memlst = []
 
--- utilities/videoviewer_writer_nobuffer.py
+++ utilities/videoviewer_writer_nobuffer.py
@@ -1,3 +1,12 @@
+'''
+python script that reads a video (or camera) and writes output to file. It does so by reading a frame and writing to disk, without buffering.
+created for testing purposes.
+
+usage:
+videoviewer_writer_nobuffer.py input_video_file.avi output_video_file.avi
+
+part of sphere-training repo: https://github.com/IIBM/sphere-training/
+'''
 import numpy as np
 import cv2
 import sys
@@ -10,6 +19,12 @@ if (len(sys.argv)) > 1:
     input_video = sys.argv[1]
     if (len(sys.argv)) > 2:
         output_video = sys.argv[2]
+else:
+    print "Not enough arguments."
+    print """usage:
+    videoviewer_writer_nobuffer.py input_video_file.avi output_video_file.avi
+    """
+    sys.exit()
 
 print "input video: ", input_video
 if (output_video == ""):
@@ -23,15 +38,17 @@ if (len(input_video) == 1):
     print "Input regarded as camera #", input_video
 cap = cv2.VideoCapture( input_video )
 # Define the codec and create VideoWriter object
-#fourcc = cv2.VideoWriter_fourcc(*'PIM1') #1.0MB
-fourcc = cv2.VideoWriter_fourcc(*'MJPG') #6.4MB
-#fourcc = cv2.VideoWriter_fourcc(*'DIV3') #3.1MB
-#fourcc = cv2.VideoWriter_fourcc(*'DIVX') #0.9MB
+##fourcc = cv2.VideoWriter_fourcc(*'PIM1') #1.0MB
+#fourcc = cv2.VideoWriter_fourcc(*'MJPG') #6.4MB
+##fourcc = cv2.VideoWriter_fourcc(*'DIV3') #3.1MB
+##fourcc = cv2.VideoWriter_fourcc(*'DIVX') #0.9MB
+fourcc = cv2.VideoWriter_fourcc(*'FFV1') #3.7MB
 
 capturedWidth = int(cap.get(3))
 capturedHeight = int(cap.get(4))
+capturedFPS = int(cap.get(5))
 
-out = cv2.VideoWriter(output_video,fourcc, 20.0, (capturedWidth,capturedHeight))
+out = cv2.VideoWriter(output_video,fourcc, capturedFPS, (capturedWidth,capturedHeight))
 #out = cv2.VideoWriter('output.avi', -1, 20.0, (640,480))
 
 
